12c42c39361170ff7dab148ddad7b0515035a50b
Indu-H <167083243+Indu-H@users.noreply.github.com>
2024-05-08 14:09:51 +0530

    RunnerUp.py

---
+++ RunnerUp.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    q = list(set(arr))#reamove the duplicates
+    q.sort()#sorted it in ascending order
+    print(q[len(q)-2])#print(q[-2])
+
+#Input (stdin)
+#5
+#2 3 6 6 5
+#Expected Output
+#5
